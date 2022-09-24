6e5b8904f7d3302a012618ebbfbcda68a82dda52
Jannatul Ferdous <110571832+nori0227@users.noreply.github.com>
2022-09-24 16:32:36 -0400

    Create descriptive_statistics.py

---
+++ descriptive_statistics.py
@@ -0,0 +1,10 @@
+#### Python Statistics Fundamentals: How to Describe Your Data ####
+ 
+ 
+ 
+#### Calculating descriptive statistics #####
+import math
+import statistics
+import numpy as np
+import scipy.stats
+import pandas as pd
